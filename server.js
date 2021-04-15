fbdb769fee670d344d03e927d93af5dfddcd4767
Emily Wilbur <ewilbur94@gmail.com>
2021-04-15 16:53:43 -0400

    server.js get

--- server.js
+++ server.js
@@ -10,3 +10,7 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
 });
+
+app.get('*', function(req,res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+});
