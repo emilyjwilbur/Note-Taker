7ebee108493576c62439c84582cd40b6f35a57e0
Emily Wilbur <ewilbur94@gmail.com>
2021-04-15 18:00:19 -0400

    module export routes file

--- routes/routes.js
+++ routes/routes.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 
-
+module.exports = app => {
 
 
 
@@ -15,3 +15,4 @@ app.get('/notes', function(req, res) {
   app.get('*', function(req,res) {
     res.sendFile(path.join(__dirname, "../public/index.html"));
 });
+}
